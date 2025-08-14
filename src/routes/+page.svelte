1f5530578ab658ca2a8ad1a5e140587f45dda408
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-08-14 13:08:10 -0300

    Monitor catalog UPDATED

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,66 +32,31 @@
   }
 
   const monitors = [
-    { code: "LC0032", title: "SEMINOVO 19' POL. *SORTIDO*", price: "R$390,00" },
-    { code: "LC0015", title: "MYMAX 19' POL., ROSA, LED", price: "R$550,00" },
-    { code: "LC0033", title: "BLUECASE 19' POL. 75Hz", price: "R$570,00" },
-    {
-      code: "LC0010",
-      title: "3GREEN 20' POL, LED, 2MS, 75HZ",
-      price: "R$630,00",
-    },
-    {
-      code: "LC0093",
-      title: "OFFICE BRIGHT, 24' POL., FULL HD, 5MS",
-      price: "R$850,00",
-    },
-    { code: "LC0014", title: "3GREEN 24' POL. 75Hz", price: "R$890,00" },
-    {
-      code: "LC0077",
-      title: "OFFICE BRIGHT, 27' POL., FULL HD, 5MS, IPS",
-      price: "R$1.090,00",
-    },
-    {
-      code: "LC0066",
-      title: "BLUECASE OPTIGAMER FHD 23.8', 100HZ, 5MS, CURVO",
-      price: "R$1.100,00",
-    },
-    {
-      code: "LC0066B",
-      title: "BLUECASE OPTIGAMER PRO 23,8' FHD, BRANCO, CURVO, 100HZ",
-      price: "R$1.190,00",
-    },
-    {
-      code: "LC00",
-      title: "BLUECASE ERAS FHD 23.8', 100HZ, CURVO",
-      price: "R$1.190,00",
-    },
-    {
-      code: "LC00",
-      title: "BLUECASE ERAS FHD 23.8', 100HZ, CURVO  *BRANCO*",
-      price: "R$1.290,00",
-    },
-    {
-      code: "LC0094",
-      title: "BLUECASE ARCUS FHD 23.6', 180HZ, CURVO",
-      price: "R$1.490,00",
-    },
-    { code: "LC0031", title: "DUEX 27' POL. 240HZ", price: "R$1.590,00" },
-    {
-      code: "LC00",
-      title: "BLUECASE ERAS FHD 27', 100HZ, CURVO  *PRETO*",
-      price: "R$1.590,00",
-    },
-    {
-      code: "LC00",
-      title: "BLUECASE ERAS FHD 27', 100HZ, CURVO  *BRANCO *",
-      price: "R$1.690,00",
-    },
-    {
-      code: "LC00",
-      title: "GRASEP 30'POL. 2K, 100HZ, ULTRAWIDE",
-      price: "R$1.890,00",
-    },
+    { code: "LC0032", title: "SEMINOVO 19' POL. *SORTIDO*", price: "R$390,00"},
+    { code: "LC0015", title: "MYMAX 19' POL., ROSA, LED", price: "R$550,00"},
+    { code: "LC0081", title: "MONITOR VX PRO 19' POL", price: "R$550,00"},
+    { code: "LC0108", title: "MONITOR VX PRO 21.5' POL., LED, 60HZ", price: "R$650,00"},
+    { code: "LC0038",title: "MONITOR BLUECASE 21.5' BRANCO, LED, 75Hz", price: "R$700,00"},
+    { code: "LC0084", title: "MONITOR MYMAX 21,5' POL. ROSA, FULL HD", price: "R$790,00"},
+    { code: "LC0109", title: "MONITOR VX PRO 23' POL", price: "R$800,00"},
+    { code: "LC0105", title: "MONITOR BLUECASE 23.6', BRANCO", price: "R$800,00"},
+    { code: "LC0014", title: "MONITOR BLUECASE 23.6', BRANCO", price: "R$990,00"},
+    { code: "LC0110", title: " MONITOR VX PRO 24' POL., LED 60HZ", price: "R$900,00"},
+    { code: "LC0077", title: "MONITOR OFFICE BRIGHT, 27' POL 5MS", price: "R$1.050,00"},
+    { code: "LC0066", title: "MONITOR BLUECASE OPTIGAMER PRO 23,8' PRETO, CURVO, 100HZ", price: "R$1.090,00"},
+    { code: "LC0066B", title: "MONITOR BLUECASE OPTIGAMER PRO 23,8' BRANCO, CURVO, 100HZ", price: "R$1.190,00"},
+    { code: "LC0096", title: "MONITOR GAMER BLUECASE ERAS 23.8' POL., 100HZ, PRETO, CURVO", price: "R$1.190,00"},
+    { code: "LC0095", title: "MONITOR GAMER BLUECASE ERAS 23.8' POL., 100HZ, BRANCO, CURVO", price: "R$1.290,00"},
+    { code: "LC0078", title: "MONITOR GAMER BLUECASE ERAS 23.8' POL., 100HZ, BRANCO, CURVO", price: "R$1.390,00", },
+
+    { code: "LC0107", title: "MONITOR ACER NITRO KG1 180HZ", price: "R$1.390,00", },
+    { code: "LC0094", title: "MONITOR GAMER BLUECASE ARCUS 23.8' POL CURVO, 180HZ", price: "R$1.490,00", },
+    { code: "LC0085", title: "MONITOR GAMER BLUECASE TAURUS 23,8, IPS, 180HZ", price: "R$1.450,00", },
+    { code: "LC0103", title: "MONITOR GRASEP LED 27', 100HZ 1MS", price: "R$1.490,00", },
+    { code: "LC0087", title: "MONITOR GAMER BLUECASE TAURUS PRO 23.8, IPS, 180HZ", price: "R$1.550,00", },
+    { code: "LC0097", title: "MONITOR GAMER BLUECASE ERAS 27' POL., PRETO, CURVO 100HZ", price: "R$1.590,00", },
+    { code: "LC0098", title: "MONITOR GAMER BLUECASE ERAS 27' POL., BRANCO, CURVO 100HZ", price: "R$1.690,00", },
+    { code: "LC0082", title: "GRASEP 30'POL. 2K, 100HZ, ULTRAWIDE", price: "R$1.890,00", },
   ];
 
   const gpus = [
