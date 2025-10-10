82104d4d503e901722a3dd59c7c1adafad5c0871
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-10-10 11:17:46 -0300

    Add product image and update title for GAMER RYZEN 5600GT to include product code

--- src/routes/amd/+page.svelte
+++ src/routes/amd/+page.svelte
@@ -8,6 +8,7 @@
       image: "PCAMD.png",
       text: "*Máquina ideal para Jogos como:* \n🚔 GTA 5/RP\n💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🔫 VALORANT\n🔫 CS2n\n*Placa Mãe:* A520\n*Processador:* RYZEN 3 3200G\n*Memória RAM:* 2x8GB(16GB) DDR4\n*Placa de Vídeo:* VEGA 8 (INTEGRADO)\n*Armazenamento:* SSD 240GB\n*Gabinete:* GAMER LED C/3 FANS RGB \n*Fonte:* 500W\n\nPOR APENAS *R$2.290,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento\n\n",
     },
+    
     // R$ 2.690,00
     {
       title: "(PC0001) GAMER RYZEN 5600GT",
