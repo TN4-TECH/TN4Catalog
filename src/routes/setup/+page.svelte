5b87ce034a787cb23d44574f8398d5e4d0145d01
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-10-13 17:24:18 -0300

    Update title for RYZEN 3 3200G setup to include product code

--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -32,7 +32,7 @@
 
     // R$3.090,00
     {
-      title: "🖥🖱SETUP GAMER RYZEN 3 3200G",
+      title: "(PC0091) SETUP GAMER RYZEN 3 3200G",
       image: "PC0014.png",
       text: "*Setup ideal para Jogos como:* \n🚔 GTA 5/RP\n💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🔫 VALORANT\n🔫 CS2\n\n*Placa Mãe:* A520\n*Processador:* RYZEN 3 3200G\n*Memória RAM:* 2x8GB(16GB) DDR4\n*Placa de Vídeo:* VEGA 8 (INTEGRADO)\n*Armazenamento:* SSD 240GB\n*Gabinete:* GAMER LED C/3 FANS RGB\n*Fonte:* 500W\n+ 🖥️ Monitor de 19' Pol.\n+ Kit Gamer(Teclado, Mouse, Mousepad e Headset)\n\nPOR APENAS *R$3.090,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento\n\n"
     },
