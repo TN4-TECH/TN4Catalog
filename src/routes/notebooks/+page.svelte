35cc1ff62a30c7459c045aaea0654d2eac369676
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2026-04-13 10:08:55 -0300

    confirmar

--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -47,6 +47,13 @@
       text: "💻 NOTEBOOK HP ELITEBOOK 840\n\n*Processador:* i5 (11ªGEN)\n*Memória RAM:* 8GB DDR4 \n*Armazenamento:* SSD DE 256GB\n*Tela:* TOUCHSCREEN.\n(Acompanha Fonte)\n\n*POR APENAS R$ 2.890,00* em até 10x sem juros!\n\n*PROPOSTA VÁLIDA POR 48 HORAS, SUJEITA A ALTERAÇÃO DE VALOR*",
     },
 
+    //R$2.690,00
+    { 
+      title: "(NT0099)   NOTEBOOK LENOVO B330  ",
+      image: "NT0099.png",
+      text: "💻 NOTEBOOK LENOVO B330\n\n*Processador:* i5 (8ªGEN)\n*Memória RAM:* 8GB DDR4 \n*Armazenamento:* SSD DE 256GB\n*Tela:* 15.6' POL.\n(Acompanha Fonte)\n\n*POR APENAS R$ 2.690,00* em até 10x sem juros!\n\n*PROPOSTA VÁLIDA POR 48 HORAS, SUJEITA A ALTERAÇÃO DE VALOR*",
+    },
+
   ];
 
   function copyToClipboard(product: { text: string }) {
