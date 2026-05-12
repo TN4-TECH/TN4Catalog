f1500c4875922f702eef3431df35e8d1db27cbba
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2026-05-12 11:33:15 -0300

    adiciona novo setup gamer RYZEN 7 5800XT + RTX 5060TI à lista de produtos

--- src/routes/pcCustom/+page.svelte
+++ src/routes/pcCustom/+page.svelte
@@ -51,6 +51,14 @@
       text: "*Máquina ideal para:* \n💥 FREE FIRE\n⚽ EA FC 26\n🔫 VALORANT\n🚔 GTA 5/RP\n🔫 CS2\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* RYZEN 7 5700G\n*Placa Mãe:* B550ZGS DUEX\n*Memória RAM:* 2x8(16GB) 3200MHZ XPG BRANCA DDR4\n*Cooler:* PCYES FROST PULSE ARGB WHITE GHOST\n*Placa de Vídeo:* RTX 5050 8GB\n*Armazenamento:* SSD NVME 500GB PRODRIVE\n*Gabinete:* 'AQUARIO' BLUECASE C/5 FANS RGB\n*Fonte:* 500W FORTREK BRANCA\n\nPOR APENAS *R$ 8.990,00* em até 10x sem juros!\n\nFormas de Pagamento:\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
     },
 
+    // R$ 10.990,00
+    {
+      title: "PC BRANCO RYZEN 7 5800XT+ RTX 5060TI",
+      image: "CUSTOM2404.png",
+      text: "*Máquina ideal para:* \n💥 FREE FIRE\n⚽ EA FC 26\n🔫 VALORANT\n🚔 GTA 5/RP\n🔫 CS2\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* RYZEN 7 5800XT\n*Placa Mãe:* B550 AORUS ELITE\n*Memória RAM:* 1X16GB 3200MHZ XPG BRANCA DDR4\n*Cooler:* AIR COOLER DRAXEN DUAL FANS DN31\n*KIT FANS FORTREK FROSTY (BRANCO)\n*Placa de Vídeo:* RTX 5060 TI 8GB\n*SUPORTE PLACA DE VIDEO ARGB REDRAGON (BRANCO)\n*Armazenamento:* SSD NVME 1TB\n*Gabinete:* ADATA XPG STARKER (BRANCO)\n*Fonte:* 600W 80PLUS\n\nPOR APENAS *R$ 10.990,00* em até 10x sem juros!\n\nFormas de Pagamento:\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
+    },
+
+
     // R$ 11.290,00
     {
       title: "PC KALKAN I7 12700F + RTX 5060",
