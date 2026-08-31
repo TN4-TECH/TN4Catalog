516808c82c28e9b0861d233c5493abe3fe5037ec
deividtn4 <DeividTN4@gmail.com>
2026-08-31 15:30:14 -0300

    att full

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,13 +31,13 @@
     open = !open;
   }
 
-  //const monitors = [
+  const monitors = [
     //{ code: "LC0032", title: "MONITOR SEMINOVO 19' POL. SORTIDO", price: "R$390,00" },
     //{ code: "LC0016", title: "MONITOR HAYOM 17.1, VGA, HDMI, PRETO", price: "R$439,90" },
-    //{ code: "LC0139", title: "MONITOR HAYOM 19' POL., VGA, HDMI, PRETO", price: "R$490,00" },
+    { code: "LC0139", title: "MONITOR HAYOM 19' POL., VGA, HDMI, PRETO", price: "R$490,00" },
     //{ code: "LC0081", title: " MONITOR VX PRO 21' POL., LED, VGA + HDMI, 60HZ", price: "R$650,00" },
     //{ code: "LC0120", title: "MONITOR MNBOX 20' POL, LED", price: "R$590,00" },
-  //]
+  ]
 
   const gpus = [
     { code: "VG0042", title: "GT 730, 4GB", price: "R$ 499,99"},
